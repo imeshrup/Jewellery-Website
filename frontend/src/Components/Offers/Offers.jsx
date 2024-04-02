ad9310b8091a459e98c12f5a78b959816af6e234
deshan005 <deshanw77@gmail.com>
2024-04-02 14:34:24 +0530

    added the offers

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Offers.css'
+import React from 'react';
+import './Offers.css';
+import exclusive_image from '../Assets/exclusive_image.png'
 
 const Offers = () => {
   return (
     <div className='offers'>
-      
+      <div className="offers-left">
+        <h1>EXCLUSIVE</h1>
+        <h1>offers for you</h1>
+        <p>ONLY BEST SELLER PRODUCTS</p>
+        <button>Check Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusive_image} alt="" />
+      </div>
     </div>
   )
 }
