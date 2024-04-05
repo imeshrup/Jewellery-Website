0f71ebdfc0e5ab2969324562c09ecad57d69f4a6
deshan005 <deshanw77@gmail.com>
2024-04-05 18:19:10 +0530

    added 3 types of media queries for 3 different sizes and adjusted the responsive bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import dropdown from '../Assets/dropdown.jpg'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
 
@@ -9,6 +10,12 @@ const Navbar = () => {
 
     const [menu,setMenu] = useState("shop");
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle= (e)=>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
@@ -16,7 +23,8 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={()=>setMenu("shop")}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>setMenu("rings")}><Link style={{textDecoration: 'none'}} to='/rings'>Rings</Link> {menu==="rings"?<hr/>:<></>}</li>
         <li onClick={()=>setMenu("necklaces")}><Link style={{textDecoration: 'none'}} to='/necklaces'>Necklaces</Link> {menu==="necklaces"?<hr/>:<></>}</li>
