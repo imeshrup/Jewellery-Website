6dbb88c8b5238ac327fb957c5f68c74e03bdba01
deshan005 <deshanw77@gmail.com>
2024-04-19 16:03:14 +0530

    cretaed the all products endpoint

--- backend/index.js
+++ backend/index.js
@@ -106,6 +106,25 @@ app.post('/addproduct',async(req,res)=>{
     })
 })
 
+//Creating API for deleting products
+
+app.post('/removeproduct', async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed")
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creating API for getting all products
+
+app.get('/allproducts', async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All products fetched")
+    res.send(products)
+})
+
 
 app.listen(port, (error)=>{
     if(!error){
